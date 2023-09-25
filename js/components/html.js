2cb765d01d7340bbfb3e300bc6d48cd7a04c5324
Emin Mamedov <emin451234@gmail.com>
2023-09-25 22:08:21 +0400

    Animation

--- js/components/html.js
+++ js/components/html.js
@@ -1,5 +1,5 @@
 const game_card = (item) => {return `
-<div class="col-12 col-md-6 game-card" tabindex="0">
+<div class="col-12 col-md-6 game-card pop-in-custom" tabindex="0" style="--pop-in: -10px;">
 <div class="content">
     <div class="front">
         <div class="image">
